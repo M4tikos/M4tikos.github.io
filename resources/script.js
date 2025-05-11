cc65b6f04c054128ce59b92ee0f5a8c7514f0d5e
M4tikos <mateuszba2007@gmail.com>
2025-05-11 16:39:55 +0200

    0.05

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,18 @@
 const account_menu = document.getElementById("account-menu");
 const account_icon = document.getElementById("account-icon");
 
-document.addEventListener('mousedown', function(e) {
+document.addEventListener('mouseup', function(e) {
     if (account_icon.contains(e.target)) {
-        if (account_menu.style.height == "0px") {
-            account_menu.style.height = "30em";
+        if (account_menu.style.top == "39px" && !account_menu.contains(e.target)) {
+            account_menu.style.top = "-440px";
+            account_menu.style.opacity = "0";
         } else {
-            account_menu.style.height = "0px";
+            account_menu.style.top = "39px";
+            account_menu.style.opacity = "1";
         }
     } else if (!account_menu.contains(e.target)) {
-        account_menu.style.height = "0px";
+        account_menu.style.top = "-440px";
+        account_menu.style.opacity = "0";
     }
 });
 
