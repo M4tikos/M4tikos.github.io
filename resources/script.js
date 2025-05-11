bbc00b1032bbf88708de7f7d4e10764f6081b64d
M4tikos <mateuszba2007@gmail.com>
2025-05-11 19:36:14 +0200

    fix

--- resources/script.js
+++ resources/script.js
@@ -2,18 +2,14 @@ const account_menu = document.getElementById("account-menu");
 const account_icon = document.getElementById("account-icon");
 
 document.addEventListener('mouseup', function(e) {
-    if (account_icon.contains(e.target) && !account_menu.contains(e.target)) {
-        if (account_menu.style.opacity == "1") {
-            account_menu.style.opacity = "0";
-            account_menu.style.fontSize = "0.8em";
-        } else {
+    if (!account_menu.contains(e.target)) {
+        if (account_icon.contains(e.target) && account_menu.style.opacity != "1") {
             account_menu.style.opacity = "1";
             account_menu.style.fontSize = "1em";
+        } else if (account_menu.style.opacity == "1") {
+            console.log("a");
+            account_menu.style.opacity = "0";
+            account_menu.style.fontSize = "0.8em";
         }
-    } else if (account_menu.style.opacity == "1") {
-        account_menu.style.opacity = "0";
-        account_menu.style.fontSize = "0.8em";
     }
 });
-
-
