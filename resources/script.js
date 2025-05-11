7d1d2bef00a9d34cc0bf66e108cd4db902fbed19
M4tikos <mateuszba2007@gmail.com>
2025-05-11 19:19:28 +0200

    0.06

--- resources/script.js
+++ resources/script.js
@@ -2,17 +2,17 @@ const account_menu = document.getElementById("account-menu");
 const account_icon = document.getElementById("account-icon");
 
 document.addEventListener('mouseup', function(e) {
-    if (account_icon.contains(e.target)) {
-        if (account_menu.style.top == "39px" && !account_menu.contains(e.target)) {
-            account_menu.style.top = "-440px";
+    if (account_icon.contains(e.target) && !account_menu.contains(e.target)) {
+        if (account_menu.style.opacity == "1") {
             account_menu.style.opacity = "0";
+            account_menu.style.fontSize = "0.8em";
         } else {
-            account_menu.style.top = "39px";
             account_menu.style.opacity = "1";
+            account_menu.style.fontSize = "1em";
         }
-    } else if (!account_menu.contains(e.target)) {
-        account_menu.style.top = "-440px";
+    } else if (account_menu.style.opacity == "1") {
         account_menu.style.opacity = "0";
+        account_menu.style.fontSize = "0.8em";
     }
 });
 
