bb344500b555c59bd21198766adffa946d744c62
M4tikos <mateuszba2007@gmail.com>
2025-05-13 18:47:14 +0200

    0.06

--- resources/script.js
+++ resources/script.js
@@ -1,15 +1,21 @@
 const account_menu = document.getElementById("account-menu");
-const account_icon = document.getElementById("account-icon");
 
 document.addEventListener('mouseup', function(e) {
-    if (!account_menu.contains(e.target)) {
-        if (account_icon.contains(e.target) && account_menu.style.opacity != "1") {
-            account_menu.style.opacity = "1";
-            account_menu.style.fontSize = "1em";
-        } else if (account_menu.style.opacity == "1") {
-            console.log("a");
-            account_menu.style.opacity = "0";
-            account_menu.style.fontSize = "0.8em";
-        }
+    if (!account_menu.contains(e.target) && account_menu.style.opacity == "1") {
+        account_menu.style.opacity = "0";
+        account_menu.style.fontSize = "0.8em";
+        account_menu.style.pointerEvents = "none";
     }
 });
+
+function openUserMenu() {
+    if (window.getComputedStyle(account_menu).opacity != "1") {
+        account_menu.style.opacity = "1";
+        account_menu.style.fontSize = "1em";
+        account_menu.style.pointerEvents = "all";
+    }
+}
+
+function openMobileNavMenu() {
+
+}
